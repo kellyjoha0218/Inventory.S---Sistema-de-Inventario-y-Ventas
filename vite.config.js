adefdc80e500339bd27b82a9c455eab4766b000b
kellyjoha656 <kellyjoha656@gmail.com>
2024-11-27 07:25:38 -0500

    Fix server configuration for Vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 3000, // Cambia el puerto si es necesario
+  },
+});
+
+
